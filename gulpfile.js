fdfb64b8f3766990e2e824e86316b28908edeacc
LaurenceWiles <140092148+LaurenceWiles@users.noreply.github.com>
2024-11-25 15:56:36 +0100

    purging css

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,17 @@
 const { src, dest, watch, series } = require("gulp");
 const sass = require("sass");
 const gulpSass = require("gulp-sass")(sass);
+const purgecss = require("gulp-purgecss");
 
 const buildStyles = () => {
   return src("shinobi/**/*.scss")
     .pipe(gulpSass().on("error", gulpSass.logError))
+    .pipe(purgecss({ content: ["*.html"] }))
     .pipe(dest("css"));
 };
 
 const watchTask = () => {
-  watch(["shinobi/**/*.scss"], buildStyles);
+  watch(["shinobi/**/*.scss", "*.html"], buildStyles);
 };
 
 exports.default = series(buildStyles, watchTask);
